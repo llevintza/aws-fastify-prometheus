3eac5885369b19bb48d768fc30d2d745689d8d19
llevintza <llevintza@gmail.com>
2025-07-31 23:36:07 -0400

    chore: remove old ESLint configuration files and add new consolidated ESLint config

--- .eslintrc.test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['@typescript-eslint/recommended'],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-};
